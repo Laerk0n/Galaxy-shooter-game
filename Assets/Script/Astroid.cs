340b2d90578b6bdc2265c7d261a8ceb3c847613f
Laerk0n <henrik.larkfeldt@gmail.com>
2023-09-06 05:54:49 +0200

    försöker lägga in en roterande astroid

---
+++ Assets/Script/Astroid.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Astroid : MonoBehaviour
+{
+    public float _rotationSpeed = 5f;
+    public float _speed = 5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.down * _speed * Time.deltaTime);
+        transform.Translate(Vector3.down * _rotationSpeed * Time.deltaTime);
+        // måste få astroiden att rotera.
+    }
+}
